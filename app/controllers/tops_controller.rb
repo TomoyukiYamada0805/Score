1d18bf88bbe41b75d67f7e32798b38496eca322a
Tomoyuki Yamada <b.vopen_59@i.softbank.jp>
2019-04-20 14:20:23 +0900

    use environment variable

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -3,13 +3,12 @@ class TopsController < ApplicationController
   def hello
 
     if user_signed_in?
-      # 後で、matchesテーブルを通してjoinし、2チームのチーム名まで取得す方法を検討する
-      #@matches = EvaluateMatch.select("*").left_outer_joins(:match).left_outer_joins(:user).all.map{ |p| p.attributes }
       @match = EvaluateMatch.select("matches.*, teams.*, users.id as user_id, users.user_name, users.uid, users.avatar, teams.team_name as home_team_name, away_teams_evaluate_matches.team_name as away_team_name").left_outer_joins(:match).left_outer_joins(:user).left_outer_joins(:home_team).left_outer_joins(:away_team)
       @like_count = []
       @match.each do |match|
         @like_count.push(Like.where(post_user_id: match[:uid], match_id: match[:match_id]).size)
       end
+      logger.info(ENV['TWITTER_KEY'])
       render "index"
     end
   end
