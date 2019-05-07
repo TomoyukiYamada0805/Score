5dac2dcc496f118d3c16175dcb3710a96805b618
Tomoyuki Yamada <b.vopen_59@i.softbank.jp>
2019-05-08 00:39:22 +0900

    change avatar info

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -6,7 +6,9 @@ class TopsController < ApplicationController
 
     @match = EvaluateMatch.select("matches.*, teams.team_name, users.id as user_id, users.user_name, users.uid, users.avatar, teams.short_name as home_team_name, away_teams_evaluate_matches.short_name as away_team_name, evaluate_matches.updated_at as evaluate_date").left_outer_joins(:match).left_outer_joins(:user).left_outer_joins(:home_team).left_outer_joins(:away_team).page(params[:page]).per(PER)
     @like_count = []
+    @user       = []
     @match.each do |match|
+      @user.push(User.where(uid: match.uid)[0])
       @like_count.push(Like.where(post_user_id: match[:uid], match_id: match[:match_id]).size)
     end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,9 @@ class UsersController < ApplicationController
       @evaluate = EvaluatePlayer.select("count(*) as count, teams.short_name, teams.team_color").where(user_id: user_id).where("evaluate_point > 0").group("teams.short_name, teams.team_color").left_outer_joins(:player).left_outer_joins(:team)
       @match = EvaluateMatch.select("matches.*, teams.*, users.id as user_id, users.user_name, users.uid, users.avatar, teams.short_name as home_team_name, away_teams_evaluate_matches.short_name as away_team_name").where(user_id: user_id).left_outer_joins(:match).left_outer_joins(:user).left_outer_joins(:home_team).left_outer_joins(:away_team).page(params[:page]).per(PER)
       @like_count = []
+      @evaluate_user = []
       @match.each do |match|
+        @evaluate_user.push(User.where(uid: match.uid)[0])
         @like_count.push(Like.where(post_user_id: match[:uid], match_id: match[:match_id]).size)
       end
       @evaluate_ranking = EvaluatePlayer.select("AVG(evaluate_point) as point, evaluate_players.player_id, players.player_name, teams.short_name").where(user_id: user_id).where("evaluate_point > 0").left_outer_joins(:player).left_outer_joins(:team).group("evaluate_players.player_id, players.player_name, `evaluate_players`.`evaluate_point`, teams.short_name").order(evaluate_point: "DESC").limit(3)
@@ -18,8 +20,9 @@ class UsersController < ApplicationController
       @like_matches = Like.where(like_user_id: user_id).page(params[:page]).per(PER)
       @match = []
       @like_count = []
-      
+      @evaluate_user = []
       @like_matches.each do |match|
+        @evaluate_user.push(User.where(uid: match.uid)[0])
         like = EvaluateMatch.select("matches.*, teams.*, users.id as user_id, users.user_name, users.uid, users.avatar, teams.short_name as home_team_name, away_teams_evaluate_matches.short_name as away_team_name").where(user_id: match.post_user_id, match_id: match.match_id).left_outer_joins(:match).left_outer_joins(:user).left_outer_joins(:home_team).left_outer_joins(:away_team)[0]
         @match.push(like)
         @like_count.push(Like.where(post_user_id: like[:uid], match_id: like[:match_id]).size)
